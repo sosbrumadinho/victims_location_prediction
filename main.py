473d6b13b55c937b69b4098b8cdaa0b684c4ca9d
Jean Matias <smatias.jean@gmail.com>
2019-02-13 02:51:31 +0000

    Clean up and pep8 lint the code

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ Underdevelopment:
 """
 import numpy as np
 import pandas as pd
-import utm 
+import utm
 from scipy.integrate import cumtrapz
 import matplotlib.pyplot as pl
 from utils.geomap import MAP
 from utils.fluid import FLUID
 
 # Example:
-# Initial position  
+# Initial position
 lat, lon = -20.137228709820736, -44.13889842358328
 # The above values are outside the region, then it gives an error
 # Thus, I used x0 and y0 inputted manually
 x0, y0, utmZoneNumber, utmZoneLetter = utm.from_latlon(lat, lon)
-x0, y0 = 590010., 7773100. # <------ manually
+x0, y0 = 590010., 7773100.  # <------ manually
 # Victim's mass
-m = 80 # kg
+m = 80  # kg
 
 # Loads fluid class
 FluidObj = FLUID()
@@ -45,14 +45,14 @@ dataSet = dataMap.Data()
 # utmArr = MDTGradient(dataMultiIndex,30)
 # print(utmArr)
 
-# Loads borders of the region 
+# Loads borders of the region
 borders = pd.read_csv('./data/borders.csv')
 # converts borders to utm and append it to original borders
-borders[ ['x0', 'y0', 'utmZoneNumber', 'utmZoneLetter'] ] = borders.apply( 
-    lambda row: pd.Series( 
-    list( utm.from_latlon( row['lat'], row['lon'] ) )
+borders[['x0', 'y0', 'utmZoneNumber', 'utmZoneLetter']] = borders.apply(
+    lambda row: pd.Series(
+        list(utm.from_latlon(row['lat'], row['lon']))
     ),
-    axis = 1 
+    axis=1
 )
 
 #####################################
@@ -60,13 +60,13 @@ borders[ ['x0', 'y0', 'utmZoneNumber', 'utmZoneLetter'] ] = borders.apply(
 dataSet['vel_u'] = dataSet['E'] * dataSet['G']
 dataSet['vel_v'] = dataSet['E'] * dataSet['H']
 
-fig, ax = pl.subplots(1, figsize = (15, 9))
+fig, ax = pl.subplots(1, figsize=(15, 9))
 q = ax.quiver(
-    dataSet['upperLeftLon'], 
-    dataSet['upperLeftLat'], 
-    dataSet['vel_u']/2, 
+    dataSet['upperLeftLon'],
+    dataSet['upperLeftLat'],
+    dataSet['vel_u']/2,
     dataSet['vel_v']/2,
-    scale = 32, 
+    scale=32,
 )
 pl.show()
 
@@ -75,8 +75,8 @@ pl.show()
 # Finds the initial position on the map
 ro = dataMap.FindPoint(x0, y0)
 lastRegion = int(20 * ro.index[0] / ro['time'].iloc[0])
-idx = ro.index[0] 
-fluidElems = dataSet.iloc[ idx : idx + lastRegion ]
+idx = ro.index[0]
+fluidElems = dataSet.iloc[idx: idx + lastRegion]
 fluidElems[['vLon', 'vLat']] = FluidObj.VelocityAt(fluidElems)
 fluidElems[['fLon', 'fLat']] = FluidObj.DriftForce()
 
@@ -99,7 +99,7 @@ positionX = cumtrapz(velocityX)
 # Integrate velocity to get y position
 positionY = cumtrapz(velocityY)
 
-# Insert initial position 
+# Insert initial position
 positionX = np.concatenate([[x0], positionX])
 positionY = np.concatenate([[y0], positionY])
 
@@ -108,11 +108,12 @@ positionX = np.cumsum(positionX)
 positionY = np.cumsum(positionY)
 
 # Gets latitude and longitude of an object in the fluid waste
-lat, lon = utm.to_latlon(positionX[-1], positionY[-1], utmZoneNumber, utmZoneLetter)
+lat, lon = utm.to_latlon(
+    positionX[-1], positionY[-1], utmZoneNumber, utmZoneLetter)
 
 print(
     '''Final position:
         lat: %f
         lon: %f
-    '''%(lat, lon)
+    ''' % (lat, lon)
 )
